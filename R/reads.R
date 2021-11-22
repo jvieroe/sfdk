d3fc48b8df5f19f85a7e0e2042dcbd9bf215c4d9
jvieroe <43220957+jvieroe@users.noreply.github.com>
2021-11-22 13:37:38 +0100

    Create reads.R

---
+++ R/reads.R
@@ -0,0 +1,4 @@
+# https://www.vejhistorie.dk/kort/historiske-kort
+# https://en.wikipedia.org/wiki/Claudius_Clavus
+# https://denstoredanske.lex.dk/Kartografiens_historie
+# https://en.wikipedia.org/wiki/Marcus_Jordanus
